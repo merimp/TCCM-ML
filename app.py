0b18cc3140fd44144e208ee74581d25b4e202aa3
merimp <113997916+merimp@users.noreply.github.com>
2022-09-20 16:07:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import streamlit as st
 
+print(st.__version__)
 st.title("This is my first app")
 
-my_text = st.text("A random version 2 of text")
 
-my_button = st.button("Run ML computation")
+my_button = st.button("Show me weather features")
 
 if my_button:
-	st.title("The model is running...")
+        col1, col2, col3 = st.columns(3)
+        col1.metric("Temperature", "70 °F", "1.2 °F")
+        col2.metric("Wind", "9 mph", "-8%")
+        col3.metric("Humidity", "86%", "4%")
 
-my_text_2 = st.text("Hola")
+
+values = st.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
+
+
+#age = st.slider('How old are you?', 0, 130, 25)
+#st.write("I'm ", age, 'years old')
+~                                  
